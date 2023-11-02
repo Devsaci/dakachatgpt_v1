bec130a690247785e345570080672a5330204507
saci zakaria <sacideveloppeur@gmail.com>
2023-11-02 21:34:46 +0100

    home: const HomeScreen(),

--- lib/main_screens/home_screen.dart
+++ lib/main_screens/home_screen.dart
@@ -10,6 +10,11 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Chat GPT"),
+      ),
+      body: const Center(child: Text("Home Screen")),
+    );
   }
 }
