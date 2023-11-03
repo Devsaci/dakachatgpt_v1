bb807f2e013e933d3d195b161e2f79a5af794096
saci zakaria <sacideveloppeur@gmail.com>
2023-11-03 11:18:40 +0100

    bool get darkTheme

--- lib/providers/my_theme_provider.dart
+++ lib/providers/my_theme_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyThemeProvider extends ChangeNotifier{
-  bool _darkTheme = false
+  bool _darkTheme = false;
+
+  bool get darkTheme => _darkTheme;
 }
