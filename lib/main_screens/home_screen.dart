619a544759bd974a988033be870cccdefe7700a3
saci zakaria <sacideveloppeur@gmail.com>
2023-11-03 14:31:37 +0100

    final themeStatus =

--- lib/main_screens/home_screen.dart
+++ lib/main_screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dakachatgpt_v1/providers/my_theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +10,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
   @override
   Widget build(BuildContext context) {
+     final themeStatus =   Provider.of<MyThemeProvider>(context);
     return  Scaffold(
       appBar: AppBar(
         title: const Text("Chat GPT"),
