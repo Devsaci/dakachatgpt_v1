cad35464a29e27e8ddeadb5fb7cba8fcfd286ecd
saci zakaria <sacideveloppeur@gmail.com>
2023-11-03 11:39:23 +0100

    Format Code

--- lib/providers/my_theme_provider.dart
+++ lib/providers/my_theme_provider.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class MyThemeProvider extends ChangeNotifier{
+class MyThemeProvider extends ChangeNotifier {
   bool _darkTheme = false;
 
   bool get darkTheme => _darkTheme;
 
-  set darkTheme(bool value)  {
+  set darkTheme(bool value) {
     _darkTheme = value;
-       saveThemeToSharedPreferences(value: value);
-
+    saveThemeToSharedPreferences(value: value);
   }
 
   void saveThemeToSharedPreferences({required bool value}) async {
     final sharedPreferences = await SharedPreferences.getInstance();
-        sharedPreferences.setBool("themeStatus", value);
+    sharedPreferences.setBool("themeStatus", value);
     notifyListeners();
   }
 }
