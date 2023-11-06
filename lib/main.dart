6b5c0722a43954f7a8aba646dc3116b7101f7758
saci zakaria <sacideveloppeur@gmail.com>
2023-11-06 11:26:34 +0100

    void initState()

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,10 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+  void initState(){
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Consumer<MyThemeProvider>(
         builder: (BuildContext context, value, Widget? child) {
