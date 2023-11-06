b5829d42253017563e2b80b3ec23607933364674
saci zakaria <sacideveloppeur@gmail.com>
2023-11-06 15:42:20 +0100

    const PostsScreen(),

--- lib/main_screens/home_screen.dart
+++ lib/main_screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dakachatgpt_v1/main_screens/posts_screen.dart';
 import 'package:dakachatgpt_v1/providers/my_theme_provider.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
   final List<Widget> tabs = [
     const AIChatScreen(),
+    const PostsScreen(),
   ];
 
   @override
