f9af884adf7786aa2439360b5a33b0baa9629d7f
saci zakaria <sacideveloppeur@gmail.com>
2023-11-07 10:47:44 +0100

    '$imagePath/user_icon.png';

--- lib/service/assets_manager.dart
+++ lib/service/assets_manager.dart
@@ -1,4 +1,4 @@
 class AssetsManager{
-  static String ImagePath = "assets/images";
-  static String userIcon = '';
+  static String imagePath = "assets/images";
+  static String userIcon = '$imagePath/user_icon.png';
 }
