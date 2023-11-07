b2eb6da2769d4a7e332808ddc5864335e2e455df
saci zakaria <sacideveloppeur@gmail.com>
2023-11-07 11:05:56 +0100

    TextFormField(),

--- lib/authentication/registration_screen.dart
+++ lib/authentication/registration_screen.dart
@@ -29,7 +29,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   ),
                   child: CircleAvatar(
                     radius: 80,
-                    backgroundImage: AssetImage(AssetsManager.userIcon),
+                    backgroundImage: AssetImage(AssetsManager.openAILogo),
                   ),
                 ),
                 const SizedBox(height: 20),
@@ -46,6 +46,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                     style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
                   ),
                 ),
+                const SizedBox(height: 20),
+                TextFormField(),
               ],
             ),
           ),
