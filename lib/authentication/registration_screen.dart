e00245686375f784ae8d11c1e4374e609582a550
saci zakaria <sacideveloppeur@gmail.com>
2023-11-07 11:00:11 +0100

    Text 'Add your phone number'

--- lib/authentication/registration_screen.dart
+++ lib/authentication/registration_screen.dart
@@ -37,6 +37,12 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   'Register',
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Add your phone number. I will send you a verification code.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
               ],
             ),
           ),
