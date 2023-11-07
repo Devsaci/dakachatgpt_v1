44280916400b267adf1ff42bcb6605a0f662336f
saci zakaria <sacideveloppeur@gmail.com>
2023-11-07 10:55:38 +0100

    backgroundImage: AssetImage(AssetsManager.openAILogo),

--- lib/authentication/registration_screen.dart
+++ lib/authentication/registration_screen.dart
@@ -29,7 +29,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   ),
                   child: CircleAvatar(
                     radius: 80,
-                    backgroundImage: AssetImage(AssetsManager.userIcon),
+                    backgroundImage: AssetImage(AssetsManager.openAILogo),
                   ),
                 ),
               ],
