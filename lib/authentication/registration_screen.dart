b76e77298b4bcddf4c112518c934dcf6a0fd6065
saci zakaria <sacideveloppeur@gmail.com>
2023-11-08 18:04:51 +0100

    void dispose()

--- lib/authentication/registration_screen.dart
+++ lib/authentication/registration_screen.dart
@@ -28,7 +28,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   final RoundedLoadingButtonController btnController =
       RoundedLoadingButtonController();
-
+@override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
